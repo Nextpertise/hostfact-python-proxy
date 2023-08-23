328d8dcd9be81bf14a4f14889ee10f7bba201ca2
Teun Ouwehand <teun@nextpertise.nl>
2023-08-22 17:29:06 -0700

    Add comment in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import sys
 import os
 
+# Add the src directory to the path.
+# This allows tests to import app from main.py in the src directory.
+# This enables: poetry run pytest
 sys.path.append(os.path.abspath("src"))
